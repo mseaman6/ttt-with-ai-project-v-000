75f5efa2ab32f8322af0189aa49380326516e00c
Monica Seaman <monica.l.seaman@gmail.com>
2018-06-19 14:22:16 +0000

    automatically backed up by learn

--- lib/players/computer.rb
+++ lib/players/computer.rb
@@ -34,15 +34,15 @@ module Players
           edges(board)
         # end
       elsif board.turn_count == 4
-        if board.taken?(2) && board.taken?(5)
+        if board.taken?(2) && board.taken?(5) && !board.taken?(8)
           "8"
-        elsif board.taken?(8) && board.taken?(5)
+        elsif board.taken?(8) && board.taken?(5) && !board.taken?(2)
           "2"
-        elsif board.taken?(4) && board.taken?(5)
+        elsif board.taken?(4) && board.taken?(5) && !board.taken?(6)
           "6"
-        elsif board.taken?(6) && board.taken?(5)
+        elsif board.taken?(6) && board.taken?(5) && !board.taken?(4)
           "4"
-        elsif board.taken?(3) && board.taken?(5)
+        elsif board.taken?(3) && board.taken?(5) && !board.taken?(7)
           "7"
         # elsif board.taken?(2) && board.taken?(1)
         #   "3"
@@ -64,13 +64,13 @@ module Players
           (rand(9) + 1).to_s
         end
       elsif board.turn_count == 5
-        if board.taken?(2) && board.taken?(1)
+        if board.taken?(2) && board.taken?(1) && !board.taken?(3)
           "3"
-        elsif board.taken?(4) && board.taken?(1)
+        elsif board.taken?(4) && board.taken?(1) && !board.taken?(7)
           "7"
-        elsif board.taken?(6) && board.taken?(9)
+        elsif board.taken?(6) && board.taken?(9) && !board.taken?(3)
           "3"
-        elsif board.taken?(8) && board.taken?(9)
+        elsif board.taken?(8) && board.taken?(9) && !board.taken?(7)
           "7"
         # elsif board.taken?(2) && board.taken?(5)
         #   "8"
@@ -84,13 +84,13 @@ module Players
           (rand(9) + 1).to_s
         end
       elsif board.turn_count == 6
-        if board.taken?(3) && board.taken?(5)
+        if board.taken?(3) && board.taken?(5) && !board.taken?(7)
           "7"
-        elsif board.taken?(7) && board.taken?(5)
+        elsif board.taken?(7) && board.taken?(5) && !board.taken?(3)
           "3"
-        elsif board.taken?(6) && board.taken?(5)
+        elsif board.taken?(6) && board.taken?(5) && !board.taken?(4)
           "4"
-        elsif board.taken?(4) && board.taken?(5)
+        elsif board.taken?(4) && board.taken?(5) && !board.taken?(6)
           "6"
         else
           (rand(9) + 1).to_s
